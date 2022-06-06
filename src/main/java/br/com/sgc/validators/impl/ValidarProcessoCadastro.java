116e8a43d60e984af5aff29148da4099c2c324ae
bpastorelli <brunopastorelli@hotmail.com>
2022-06-06 17:03:10 -0300

    Validação

--- src/main/java/br/com/sgc/validators/impl/ValidarProcessoCadastro.java
+++ src/main/java/br/com/sgc/validators/impl/ValidarProcessoCadastro.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 import br.com.sgc.dto.MoradorDto;
 import br.com.sgc.dto.ProcessoCadastroDto;
-import br.com.sgc.dto.ResidenciaDto;
+import br.com.sgc.errorheadling.ErroRegistro;
 import br.com.sgc.errorheadling.RegistroException;
 import br.com.sgc.validators.Validators;
 
@@ -18,18 +18,35 @@ public class ValidarProcessoCadastro implements Validators<ProcessoCadastroDto> 
 	@Autowired
 	private Validators<List<MoradorDto>> validarMorador;
 	
-	@Autowired
-	private Validators<ResidenciaDto> validarResidencia;
+	private static final String TITULO = "Processo de cadastro recusado!";
 	
 	@Override
 	public void validar(ProcessoCadastroDto t) throws RegistroException {
+		
+		RegistroException errors = new RegistroException();
 		
 		List<MoradorDto> moradores = new ArrayList<MoradorDto>();
 		moradores.add(t.getMorador());
 		
 		this.validarMorador.validar(moradores);
 		
-		this.validarResidencia.validar(t.getResidencia());
+		if(t.getResidencia().getEndereco().isBlank() || t.getResidencia().getEndereco().isEmpty())
+			errors.getErros().add(new ErroRegistro("", TITULO, " Campo endereço é obrigatório!")); 
+		
+		if(t.getResidencia().getNumero() == 0L || t.getResidencia().getNumero() == null)
+			errors.getErros().add(new ErroRegistro("", TITULO, " Campo número é obrigatório!")); 
+		
+		if(t.getResidencia().getCep().isBlank() || t.getResidencia().getCep().isEmpty())
+			errors.getErros().add(new ErroRegistro("", TITULO, " Campo CEP é obrigatório!"));
+		
+		if(t.getResidencia().getCidade().isBlank() || t.getResidencia().getCidade().isEmpty())
+			errors.getErros().add(new ErroRegistro("", TITULO, " Campo Cidade é obrigatório!"));
+		
+		if(t.getResidencia().getUf().isBlank() || t.getResidencia().getUf().isEmpty())
+			errors.getErros().add(new ErroRegistro("", TITULO, " Campo UF é obrigatório!"));
+		
+		if(!errors.getErros().isEmpty())
+			throw errors;
 		
 	}
 
