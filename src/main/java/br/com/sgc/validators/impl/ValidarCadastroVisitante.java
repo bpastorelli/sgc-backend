fd11c51acd6d76f60ad66222a418bcdacc5471e0
brunopastorelli <brunopastorelli@hotmail.com>
2023-09-21 10:23:00 -0300

    Correção de bug

--- src/main/java/br/com/sgc/validators/impl/ValidarCadastroVisitante.java
+++ src/main/java/br/com/sgc/validators/impl/ValidarCadastroVisitante.java
@@ -90,8 +90,8 @@ public class ValidarCadastroVisitante implements Validators<VisitanteDto, Atuali
 		}
 		
 		if(!t.getCpf().replace(".", "").replace("-", "").equals(visitanteSource.get().getCpf().replace(".", "").replace("-", ""))) {
-			if(this.visitanteRepository.findByCpf(t.getCpf()).isPresent())
-				errors.getErros().add(new ErroRegistro("", TITULO, " O CPF já existe"));
+			if(this.visitanteRepository.findByCpf(t.getCpf()).get().size() > 0)
+				errors.getErros().add(new ErroRegistro("", TITULO, " O CPF já existe") );
 			
 			if(!ValidaCPF.isCPF(t.getCpf()))
 				errors.getErros().add(new ErroRegistro("", TITULO, " CPF inválido"));
