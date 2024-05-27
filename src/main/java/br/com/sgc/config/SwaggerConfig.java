838a3cb189adc0d91ccd90dfff5f40e0e928f34f
brunopastorelli <brunopastorelli@hotmail.com>
2024-05-27 19:42:09 -0300

    Inseridas informações das APIs

--- src/main/java/br/com/sgc/config/SwaggerConfig.java
+++ src/main/java/br/com/sgc/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package br.com.sgc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +21,14 @@ public class SwaggerConfig {
           .select()                                  
           .apis(RequestHandlerSelectors.basePackage("br.com.sgc.controllers"))              
           .paths(PathSelectors.any())                          
-          .build();                                           
+          .build()
+          .apiInfo(apiInfo());                                           
     }
+    
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("SGC - Sistema de Gestão de Condomínios API")
+				.description("Documentação da API de acesso aos endpoints do SGC.").version("1.0")
+				.build();
+	}
 	
 }
