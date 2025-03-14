fc48a17cc0fd07d6481ac8dd6a4d9f840fec5350
bpastorelli <54052030+bpastorelli@users.noreply.github.com>
2025-03-14 16:27:44 -0300

    Update WebSecurity.java

--- src/main/java/br/com/sgc/security/config/WebSecurity.java
+++ src/main/java/br/com/sgc/security/config/WebSecurity.java
@@ -31,6 +31,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+		        .mvcMatchers(HttpMethod.GET, "*").permitAll()
         		.mvcMatchers(HttpMethod.POST, "**/sgc/token").permitAll()
         		.mvcMatchers(HttpMethod.GET, "**/sgc/access/**").permitAll()
         		.mvcMatchers(HttpMethod.PUT, "**/access/**").permitAll()
